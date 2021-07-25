44ee6189da4875d60bd47af53dfe57118c3a1a2b
Ayush Dubey <dubeyayush1687@gmail.com>
2021-07-25 22:34:23 +0530

    Updates

---
+++ precache-manifest.64b655d4b537ceab30ad2d2098018901.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed4b3445eb8a40792a06d160c59cc89",
+    "url": "/Advice-web/index.html"
+  },
+  {
+    "revision": "19870328003bcfa415f2",
+    "url": "/Advice-web/static/css/main.039a1f38.chunk.css"
+  },
+  {
+    "revision": "7d39fd5fbfe8dd9ce0fd",
+    "url": "/Advice-web/static/js/2.c5f4b641.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Advice-web/static/js/2.c5f4b641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19870328003bcfa415f2",
+    "url": "/Advice-web/static/js/main.2c48eccf.chunk.js"
+  },
+  {
+    "revision": "0cf246bdecbba14460d0",
+    "url": "/Advice-web/static/js/runtime-main.65b9db14.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/Advice-web/static/media/city.91c109fc.jpg"
+  }
+]);
